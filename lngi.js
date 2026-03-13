7243e76ba8aba9b192048ca3600ee7d4b6a88da2
RNGDelak <phamlehaibang331@gmail.com>
2026-03-13 17:36:05 +0700

    Update lngi.js

--- lngi.js
+++ lngi.js
@@ -1,3 +1,23 @@
+function map(x) {
+
+    x = new Decimal(x);
+
+    return new Decimal(1).minus(
+        new Decimal(2).div(3).pow(x)
+    );
+}
+
+// log_{2/3}(1 - x)
+function unmap(x) {
+
+    x = new Decimal(x);
+
+    return new Decimal(1).minus(x)
+        .ln()
+        .div(
+            new Decimal(2).div(3).ln()
+        );
+}
 function gen_init_bms(x) {
     x = new Decimal(x)
 
@@ -19,6 +39,21 @@ function lngi(x) {
 
     x = new Decimal(x)
 
+    // boundary conditions
+    if (x.gt(1)) return ["Lim(BMS)",new Decimal(0)]
+    if (x.lt(0)) return ["0",new Decimal(0)]
+
+    x = unmap(x)
+
+    // special case: 0 <= x < 1
+    if (x.gte(0) && x.lt(1)) {
+        let k = Decimal.floor(
+            Decimal.log(Decimal.sub(1, x), Decimal.div(2, 3))
+        ).toNumber()
+
+        return (k === 0) ? ["0",1] : ["(0)".repeat(k),new Decimal(k)]
+    }
+
     let s = gen_init_bms(x.floor())
 
     let j = x.mod(1)
@@ -26,7 +61,6 @@ function lngi(x) {
     let k = [new Decimal(1)]
 
     let l = new Decimal(0)
-
     let m = new Decimal(0)
 
     while (l.lt(Decimal.precision)) {
@@ -34,28 +68,23 @@ function lngi(x) {
         if (j.lt(0.5)) {
 
             j = j.times(2)
-
             k.push(new Decimal(0))
-
             m = m.plus(1)
 
         } else {
 
             j = j.minus(0.5).times(2)
-
             k[k.length - 1] = k[k.length - 1].plus(1)
 
             if (j.eq(0)) {
                 l = new Decimal(30)
             }
-
         }
 
         l = l.plus(1)
     }
 
     let p = new Decimal(0)
-
     let i = new Decimal(0)
 
     while (i.lte(m)) {
@@ -71,7 +100,6 @@ function lngi(x) {
         }
 
         p = p.plus(k[i]).plus(1)
-
         i = i.plus(1)
     }
 
